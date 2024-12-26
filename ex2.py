188a01b4be5eb8467c31583e0c1b7906063b899e
Krugovcov <krugovcovmatvey@gmail.com>
2024-12-26 10:38:21 +0200

    update get_cats_info

--- ex2.py
+++ ex2.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 def get_cats_info(path):
     file_path = Path(path)
-    cats = [{}]
+    cats = []
 
     if not file_path.exists():
         print(f"Файл'{file_path}' не знайдено.")
@@ -9,5 +11,19 @@ def get_cats_info(path):
     with file_path.open(mode='r', encoding='utf-8') as file:
         for line in file:
             cleared_string=(line.strip().split(','))
-    print(cleared_string)
-get_cats_info('cats.txt')
+            if len(cleared_string) == 3:
+                id, name, age = cleared_string
+                
+                try:
+                    age = int(age)
+                    cats.append({'id': id, 'name': name, 'age': age})
+                    
+                except ValueError:
+                    continue
+   
+    if not cats:
+        print("Файл не містить даних про котів або дані некоректні.")
+    return(cats)
+
+cats_info = get_cats_info('cats.txt')
+print(cats_info)
