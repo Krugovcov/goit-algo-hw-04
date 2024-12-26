1960ba276428398548bbefabd80fcbb83b36554f
Krugovcov <krugovcovmatvey@gmail.com>
2024-12-26 10:26:51 +0200

    1

--- ex2.py
+++ ex2.py
@@ -1,1 +1,13 @@
-adad
+def get_cats_info(path):
+    file_path = Path(path)
+    cats = [{}]
+
+    if not file_path.exists():
+        print(f"Файл'{file_path}' не знайдено.")
+        return None
+
+    with file_path.open(mode='r', encoding='utf-8') as file:
+        for line in file:
+            cleared_string=(line.strip().split(','))
+    print(cleared_string)
+get_cats_info('cats.txt')
